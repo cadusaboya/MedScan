958e3fa260699dbe0ee7b812b3734fef84bdb852
cadusaboya <carlosepsaboya@gmail.com>
2024-07-29 13:00:03 -0400

    Using concurrency to improve scraping speed

--- server/prices/webscrape.py
+++ server/prices/webscrape.py
@@ -10,6 +10,8 @@ import os
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
+import concurrent.futures
+
 
 def is_url_valid(url):
     try:
@@ -26,7 +28,7 @@ API_KEY = os.getenv('API_KEY')
 CX = os.getenv('CX')
 
 def get_ifood_price(product_name, cep):
-    if product_name == 'Entresto 100mg 60 comprimidos': product_name = 'Entresto 49mg + 51mg 60 comprimidos'
+    if product_name == 'entresto 100mg': product_name = 'entresto 49mg 51mg'
     options = Options()
     options.add_argument("--incognito")  # Open Chrome in incognito mode
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
@@ -166,11 +168,8 @@ def get_drogasil_price(product_name):
     return {'Drogasil': cheapest_price}
 
 def get_price_globo(product_name, company_name):
-    if product_name == 'Entresto 100mg 60 comprimidos': product_name = 'Entresto 49mg + 51mg 60 comprimidos'
-    options = Options()
-    ##options.add_argument("--headless")  # Run Chrome in headless mode
-    options.add_argument("--incognito")  # Open Chrome in incognito mode
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    if product_name == 'entresto 100mg': 
+        product_name = 'entresto 49mg 51mg'
     
     print(f"Searching for {product_name} at {company_name}")
     search_query = f"{product_name} {company_name}"
@@ -186,6 +185,7 @@ def get_price_globo(product_name, company_name):
     if 'items' in data and len(data['items']) > 0:
         for item in data['items']:
             url = item['link']
+            print(url)
             if is_url_valid(url) and url.endswith('/p') and all(keyword in url for keyword in keywords):
                 print("Valid URL found:", url)
                 valid_urls.append(url)
@@ -194,27 +194,39 @@ def get_price_globo(product_name, company_name):
         raise ValueError("No prices found for the product")
 
     prices = []
-    for url in valid_urls:
-        driver.get(url)
 
+    def process_url(url):
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--incognito")  # Open Chrome in incognito mode
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        local_prices = []
         try:
+            driver.get(url)
             # Extract the product price from the new page (Update the selector based on the website's structure)
-            # Assuming the price can be found with a class name 'price' (adjust as necessary)
             price_elements = WebDriverWait(driver, 5).until(
                 EC.presence_of_all_elements_located((By.CLASS_NAME, 'vtex-product-price-1-x-sellingPriceValue')))
             for price_element in price_elements:
                 price_text = price_element.text.strip().replace('R$', '').replace('.', '').replace(',', '.').replace('\u00a0', '')
                 if price_text:
                     try:
-                        prices.append(float(price_text))
+                        local_prices.append(float(price_text))
                         print(f"Globo found price: {price_text}")
                     except ValueError:
                         print(f"Skipping invalid price: {price_text}")
         except Exception as e:
             print(f"An error occurred while processing {url}: {e}")
+        finally:
+            driver.quit()
+        return local_prices
 
-    driver.quit()
-    
+    print("Starting to collect Globo prices...")
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(process_url, url) for url in valid_urls]
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            prices.extend(result)
+
     if prices:
         cheapest_price = min(prices)
     else:
@@ -222,11 +234,11 @@ def get_price_globo(product_name, company_name):
     
     return {'Globo': cheapest_price}
 
+
 def get_price_paguemenos(product_name, company_name):
     options = Options()
     options.add_argument("--headless")  # Run Chrome in headless mode
     options.add_argument("--incognito")  # Open Chrome in incognito mode
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     
     search_query = f"{product_name} {company_name}"
     # Make the request to the Google Custom Search API
@@ -249,29 +261,40 @@ def get_price_paguemenos(product_name, company_name):
         print("No valid URLs found.")
         raise ValueError("No prices found for the product")
 
-    print("Starting to collect prices...")
+    print("Starting to collect Pague Menos prices...")
     prices = []
-    for url in valid_urls:
-        driver.get(url)
 
+    def process_url(url):
+        local_prices = []
+        options = Options()
+        options.add_argument("--headless")  # Run Chrome in headless mode
+        options.add_argument("--incognito")  # Open Chrome in incognito mode
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         try:
+            driver.get(url)
             # Extract the product price from the new page (Update the selector based on the website's structure)
-            # Assuming the price can be found with a class name 'price' (adjust as necessary)
             price_elements = WebDriverWait(driver, 5).until(
                 EC.presence_of_all_elements_located((By.CLASS_NAME, 'vtex-store-components-3-x-currencyContainer')))  # Example selector
             for price_element in price_elements:
                 price_text = price_element.text.strip().replace('R$', '').replace('.', '').replace(',', '.').replace('\u00a0', '')
                 if price_text:
                     try:
-                        prices.append(float(price_text))
+                        local_prices.append(float(price_text))
                         print(f"Pague Menos found price: {price_text}")
                     except ValueError:
                         print(f"Skipping invalid price: {price_text}")
         except Exception as e:
             print(f"An error occurred while processing {url}: {e}")
+        finally:
+            driver.quit()
+        return local_prices
 
-    driver.quit()
-    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(process_url, url) for url in valid_urls]
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            prices.extend(result)
+
     if prices:
         cheapest_price = min(prices)
     else:
