ce3a11ec947e6d632a45833d6974b4c93f305b66
cadusaboya <carlosepsaboya@gmail.com>
2024-08-22 22:58:04 -0400

    Changed frontend to run each api call concurrently

--- swift/MedScan/ContentView.swift
+++ swift/MedScan/ContentView.swift
@@ -1,40 +1,44 @@
 import SwiftUI
+import SafariServices
 
-// Define models for both success and error responses
 struct PriceResponse: Codable {
-    let id: Int
     let name: String
-    let prices: [Price]
-    
-    struct Price: Codable, Identifiable {
-        let id: Int
-        let provider: String
-        let price: String
-        let medicine: Int
-    }
-}
-
-struct ErrorResponse: Codable {
-    let error: String
+    let lowest_price: Double
 }
 
 struct ContentView: View {
     @State private var medicineName: String = ""
-    @State private var prices: [String] = []
-    @State private var isLoading: Bool = false
-    @State private var isAvailable: Bool = true // Track availability
+    @State private var cep: String = ""
+    @State private var drogasilPrice: Double?
+    @State private var globoPrice: Double?
+    @State private var pagueMenosPrice: Double?
+    @State private var ifoodPrice: Double?
+    @State private var isLoadingDrogasil: Bool = false
+    @State private var isLoadingGlobo: Bool = false
+    @State private var isLoadingPagueMenos: Bool = false
+    @State private var isLoadingIFood: Bool = false
+    @State private var selectedProviderURL: URL?
+    @State private var showSafari: Bool = false
 
     var body: some View {
         NavigationView {
-            VStack(alignment: .center, spacing: 20.0) {
+            VStack(alignment: .center) {
                 Text("Buscador de Preço")
                     .font(.title)
                     .fontWeight(.bold)
+                    .padding(.vertical, 10.0)
+                
                 TextField("Digite o nome do remédio", text: $medicineName)
-                    .padding()
+                    .padding(.horizontal)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .autocapitalization(.none)
                 
+                TextField("Digite a cidade", text: $cep)
+                    .padding(.horizontal)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+                    .autocapitalization(.none)
+
                 Button(action: {
                     fetchPrices()
                 }) {
@@ -47,103 +51,110 @@ struct ContentView: View {
                 }
                 .padding()
 
-                if isLoading {
-                    ProgressView()
-                        .padding()
-                }
-
-                List(prices, id: \.self) { price in
+                List {
                     HStack {
-                        Text(price)
-                        Spacer()
-                        if isAvailable {
-                            Button(action: {
-                                openWebsite(for: price)
-                            }) {
-                                Text("Comprar")
-                                    .padding(8)
-                                    .background(Color.green)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(8)
-                            }
-                        }
+                        Text("Fornecedor")
+                            .frame(width: 150, alignment: .leading) // Fixed width for provider names
+                            .fontWeight(.bold)
+                        Text("Price")
+                            .frame(width: 100, alignment: .leading) // Fixed width for prices
+                            .fontWeight(.bold)
+                        Text("") // Empty space for button
+                            .frame(width: 80, alignment: .leading) // Fixed width for button space
                     }
+                    .frame(height: 50) // Fixed height for header row
+
+                    priceRow(name: "Drogasil", price: drogasilPrice, isLoading: isLoadingDrogasil, provider: "drogasil")
+                    priceRow(name: "Drogaria Globo", price: globoPrice, isLoading: isLoadingGlobo, provider: "globo")
+                    priceRow(name: "Pague Menos", price: pagueMenosPrice, isLoading: isLoadingPagueMenos, provider: "paguemenos")
+                    priceRow(name: "iFood", price: ifoodPrice, isLoading: isLoadingIFood, provider: "ifood")
                 }
             }
             .padding()
             .navigationTitle("MedScan")
             .navigationBarTitleDisplayMode(.inline)
-            .navigationBarItems(leading: EmptyView(), trailing: EmptyView()) // To center the title
+            .sheet(isPresented: $showSafari) {
+                if let url = selectedProviderURL {
+                    SafariView(url: url)
+                }
+            }
         }
     }
 
     func fetchPrices() {
-        isLoading = true
+        fetchProviderPrice(apiEndpoint: "drogasil", isLoading: $isLoadingDrogasil, price: $drogasilPrice)
+        fetchProviderPrice(apiEndpoint: "globo", isLoading: $isLoadingGlobo, price: $globoPrice)
+        fetchProviderPrice(apiEndpoint: "paguemenos", isLoading: $isLoadingPagueMenos, price: $pagueMenosPrice)
+        fetchProviderPrice(apiEndpoint: "ifood", isLoading: $isLoadingIFood, price: $ifoodPrice)
+    }
 
-        // Replace with your actual API URL
-        let urlString = "http://localhost:8000/api/medicines/\(medicineName.lowercased())/prices/"
+    func fetchProviderPrice(apiEndpoint: String, isLoading: Binding<Bool>, price: Binding<Double?>) {
+        isLoading.wrappedValue = true
+        let urlString = "http://localhost:8000/api/\(apiEndpoint)/\(medicineName.lowercased())/?cep=\(cep)"
+        
         guard let url = URL(string: urlString) else {
-            isLoading = false
+            isLoading.wrappedValue = false
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print("Error fetching data: \(error)")
-                DispatchQueue.main.async {
-                    self.isLoading = false
-                    self.prices = ["Error fetching data"]
-                    self.isAvailable = false
-                }
-                return
-            }
-            
-            guard let data = data else {
-                DispatchQueue.main.async {
-                    self.isLoading = false
-                    self.prices = ["No data received"]
-                    self.isAvailable = false
-                }
-                return
-            }
-            
-            // Print the raw JSON data for debugging
-            if let jsonString = String(data: data, encoding: .utf8) {
-                print("Raw JSON response: \(jsonString)")
-            }
-            
-            do {
-                // Attempt to decode as PriceResponse
-                self.isLoading = false
-                let decodedResponse = try JSONDecoder().decode(PriceResponse.self, from: data)
-                DispatchQueue.main.async {
-                    if decodedResponse.prices.isEmpty {
-                        self.prices = ["Sorry, this medicine is not available in our Database"]
-                        self.isAvailable = false
+            DispatchQueue.main.async {
+                isLoading.wrappedValue = false
+                if let data = data {
+                    if let decodedResponse = try? JSONDecoder().decode(PriceResponse.self, from: data) {
+                        print("Decoded \(apiEndpoint.capitalized) Response: \(decodedResponse)") // Debug print
+                        price.wrappedValue = decodedResponse.lowest_price
                     } else {
-                        self.prices = decodedResponse.prices.map { "\($0.provider): R$ \($0.price)" }
-                        self.isAvailable = true
+                        print("Failed to decode \(apiEndpoint.capitalized) response: \(String(data: data, encoding: .utf8) ?? "No data")") // Debug print
+                        price.wrappedValue = nil
                     }
+                } else {
+                    print("No data received or error for \(apiEndpoint.capitalized): \(error?.localizedDescription ?? "Unknown error")") // Debug print
+                    price.wrappedValue = nil
                 }
-            } catch {
-                self.prices = ["Sorry, this medicine is not available in our Database"]
-                self.isAvailable = false
             }
         }
-        
         task.resume()
     }
 
-    func openWebsite(for price: String) {
-        let components = price.split(separator: ":")
-        if components.count > 1 {
-            let companyName = components[0].trimmingCharacters(in: .whitespaces)
-            let urlString = "https://\(companyName.lowercased()).com.br"
-            if let url = URL(string: urlString) {
-                UIApplication.shared.open(url)
+    func priceRow(name: String, price: Double?, isLoading: Bool, provider: String) -> some View {
+        HStack {
+            Text(name)
+                .frame(width: 150, alignment: .leading) // Fixed width for provider names
+            if isLoading {
+                ProgressView()
+                    .frame(width: 100, alignment: .leading) // Fixed width for loading indicator
+            } else if let price = price {
+                Text(String(format: "R$ %.2f", price))
+                    .frame(width: 100, alignment: .leading) // Fixed width for price
+                Button(action: {
+                    guard let url = URL(string: "https://\(provider).com.br/\(medicineName.lowercased().replacingOccurrences(of: " ", with: "-"))") else { return }
+                    selectedProviderURL = url
+                    showSafari = true
+                }) {
+                    Text("Buy")
+                        .frame(width: 80, height: 40)
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            } else {
+                Text("N/A")
+                    .frame(width: 100, alignment: .leading) // Fixed width for N/A
             }
         }
+        .frame(height: 50) // Fixed height for rows
     }
+}
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+
+    func makeUIViewController(context: Context) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {}
 }
 
 struct ContentView_Previews: PreviewProvider {
